7c267cf535ff7e69b2d4feacbd4064095619c32a
Talaini <talainimohammed@gmail.com>
2023-08-10 18:41:20 +0100

    task 8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,12 @@
 """Import lib for FileStorage Class"""
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
 import os.path
 
 
