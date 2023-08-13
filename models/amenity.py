066e704ad68de8da18f0130e8dbbdbd8bc0d2aba
omazoz <oumaymamazoz12@gmail.com>
2023-08-13 16:56:00 +0100

    tasks_add

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for class Amenity
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Class for Amenity"""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for class Review
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ Class for Review"""
+    place_id = ""
+    user_id = ""
+    text = ""
