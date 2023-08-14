76d09108fa25eeb34088d217f12857d5db07c2f2
Talaini <talainimohammed@gmail.com>
2023-08-14 09:46:41 +0100

    Update console.py

--- console.py
+++ console.py
@@ -2,6 +2,12 @@
 """Module for class HBNBCommand
 """
 from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
 import cmd
 import models
 
